4faa9e78ff0eb213b774e7753bd102464f30a0b4
Nyrontip <simondavidcruzsuazo@gmail.com>
2025-09-24 22:35:38 -0500

    se creo el metodo para flujo de chat con contexto

--- src/scripts/test.ts
+++ src/scripts/test.ts
@@ -1,31 +1,3 @@
-export async function sendMessage(model: string, prompt: string) {
-  const url = "http://localhost:11434/api/generate";
-
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        model: model,
-        prompt: prompt,
-        stream: false,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const result = await response.json();
-    console.log(result);
-    return result;
-  } catch (error: any) {
-    console.error("Error:", error.message);
-  }
-}
-
 interface Message {
   role: string;
   content: string;
@@ -80,21 +52,4 @@ export async function* chat(model: string, history: Message[]) {
   } catch (error: any) {
     console.error("Error en chat():", error.message);
   }
-}
-
-export function addMessageToDOM(
-  content: string,
-  sentBy: string,
-  parent: HTMLElement,
-) {
-  const newMessageContent = document.createElement("div");
-  const newMessage = document.createElement("p");
-  newMessage.textContent = content;
-  newMessageContent.appendChild(newMessage);
-  if (sentBy === "user") {
-    newMessage.className = "user-message";
-  } else {
-    newMessage.className = "ai-message";
-  }
-  parent.appendChild(newMessageContent);
 }
